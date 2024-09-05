a14d030bd66939b75108385c7530edd85b52e581
Prudence Radebe <122433264+MissPhumy@users.noreply.github.com>
2024-09-05 19:49:46 +0200

    chore(secrets): add environment variables for secure deployment on Circumeo
    - Configured environment variables for sensitive data including CERCUMEO_KEY.
    - Ensured secure access to API keys without hardcoding them in the codebase.
    - Updated Django settings to retrieve environment variables using os.environ.

--- ImageClassify/settings.py
+++ ImageClassify/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sg^yj481vib_7y1g-yy5gff#w*x_o3dq=by1w=yxd7e3jyqb3c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
